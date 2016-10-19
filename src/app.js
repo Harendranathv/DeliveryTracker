b776743c3d0ad455cc6a687076ad9964fd59a60a
SoumyaPM <wicked0som@gmail.com>
2016-10-19 18:53:25 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 // app.js
-var deliveryApp = angular.module('deliveryTracker', ['ui.router']);
+var deliveryApp = angular.module('deliveryTracker', ['ui.router','ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
 deliveryApp.config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/reportee');
+    $urlRouterProvider.otherwise('/login');
     
     $stateProvider
         
         // HOME STATES AND NESTED VIEWS ========================================
+        
+        .state('login', {
+            url: '/login',
+            templateUrl: 'modules/login/login.html'
+        })
         .state('reportee', {
             url: '/reportee',
             templateUrl: 'modules/reportee/reportee-view.html'
+        })
+        .state('reportee.newtask', {//nested state [products is the nested state of business state]
+            url: "/newtask",
+            templateUrl: "modules/reportee/partial-newtask-reportee.html"
+            
+        })
+        .state('reportee.blockers', {//nested state [services is the nested state of business state]
+            url: "/blockers",
+            templateUrl: "modules/reportee/blockers.html"
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
